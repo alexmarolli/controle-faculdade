02f7142f6730a6fd79dbd83a1cd66c9103c81544
alexmarolli <alexmarolli24@gmail.com>
2023-06-24 01:32:36 -0300

    react init

--- front/README.md
+++ front/README.md
@@ -2,3 +2,9 @@
 
  Então estamos todos fudidos, o mockup está no figma:
     https://www.figma.com/file/ktnrNz7DPp3FjBglWoTcyn/Controle?type=design&node-id=0%3A1&mode=design&t=FfSA3W2OAhxg6n4e-1
+
+## Pessoas Queridas, Leiam
+
+    É muito mai easy só com o react de verdade 
+    tem que ver como fazer as rotas de navegação e tals mas é mais tranquilo que com o Next 
+        
--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main /*className='bg-fundo  max-h-screen'*/>
+      <header className='bg-gray-600 h:{24}'>
+        <p>teste</p>
       </header>
-    </div>
+    </main>
   );
 }
 
