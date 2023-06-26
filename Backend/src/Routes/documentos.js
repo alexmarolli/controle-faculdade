d9d9835d6b47469699e566418a3203770d1bd042
Alex_Marolli <alexmarolli24@gmail.com>
2023-06-26 17:36:50 -0300

    problemas com o documento

--- Backend/src/Routes/documentos.js
+++ Backend/src/Routes/documentos.js
@@ -3,25 +3,25 @@ import {  prisma } from '../lib/prisma.js';
 
 export async function routeDocumentos(app){
     app.get('/documents', async(request,reply)=>{
-      const {cliente_id,Pag_id} = request.query;
+      const {id_cliente,pag_id} = request.query;
       let where={};
 
-      if(cliente_id){
-        where.cliente_id= Number(cliente_id)
+      if(id_cliente){
+        where.id_cliente= Number(id_cliente)
       }
 
-      if(Pag_id){
-        where.Pag_id=Number(doc_controle)
+      if(pag_id){
+        where.Pag_id=Number(pag_id)
       }
       console.log (where)
-        const documents = await prisma.documento.findMany(
+        const documents = await prisma.documento.findMany({
           where,
-        )
+        })
         return documents
     })
 
-    app.post('/document',async (request, reply)=>{
-        const {usuarioId,id_cliente,Pag_id,saida, valor, descricao}= request.body
+    app.post('/document',async (request, reply)=>{// criação do documento 
+        const {usuarioId,id_cliente,Pag_id,saida, valor, descricao, cancelado,}= request.body
 
         const clienteExite = await prisma.cliente.findFirst({
             where: {
@@ -42,10 +42,49 @@ export async function routeDocumentos(app){
               valor,
               descricao,
               Pag_id,
-              saida
+              saida,
             },
           });
 
+          const vencimento = await prisma.forma_pag.findFirst({
+            where:{
+                pag_id:forma_pag,
+                prazo:true
+            }
+        })
+
+        if(!vencimento){
+            const financeiro = await prisma.financeiro.create({
+                data: {
+                    id_cliente,
+                    Doc_controle: novoDocumento.Doc_controle,
+                    usuarioId: usuarioId,
+                    forma_pagPag_id: forma_pag,
+                    debCred: debCred,
+                    dt_vencimento: new Date(),
+                    cancelado: cancelado,
+                    valor: valor,
+                    pago:true,
+                    dt_pago:new Date()
+                  }
+            })
+        return financeiro;
+        }else{
+            const financeiro = await prisma.financeiro.create({
+                data:{
+                    Cliente_id: cliente_id,
+                    Doc_controle: null,
+                    usuarioId: usuarioId,
+                    forma_pagPag_id: forma_pag,
+                    debCred: debCred,
+                    dt_vencimento: new Date(),
+                    cancelado: cancelado,
+                    valor: valor,
+                    pago:false,
+                    dt_pago
+                  }                    
+            })
+        }
           return novoDocumento;        
     })
 }
