5d747fe63694ce0b4cd238f586ea3a5d96b97996
alex <alexmarolli24@gmail.com>
2023-12-08 11:59:05 -0300

    Mb

--- mobile/src/screens /Home.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function Home(){
-    return(
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className='color'>MINHA ROLA</Text>
-        </View>
-    )
-}
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-
-const nativewind = require("nativewind/tailwind/css")
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{s,jsx,ts,tsx}  "
-  ],
-  plugins: [nativewind()],
+    "./App.{js,jsx,ts,tsx}", 
+    "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'fundo':'#1C2B4C',
+        'card': '#F0F0F0'
+      }
+    },
   },
   plugins: [],
 }
-
